ee95b9ddcf0171ab0afa30690ef741b1a5e00f90
Sushama <borhade.sushama95@gmail.com>
2020-12-04 14:27:42 +0530

    Commited code

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  bool newuser;
+  SharedPreferences localStorage;
+  
+  // ignore: non_constant_identifier_names
+  void check_if_already_login() async{
+    localStorage = await SharedPreferences.getInstance();
+    newuser = (localStorage.getBool('login') ?? true);
+    if(newuser == false){
+      Navigator.pushNamed(context, '/home');
+    }
+    else{
+      Navigator.pushNamed(context, '/reg');
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 7), finished);
+  }
+
+  void finished(){
+    check_if_already_login();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'LIB',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                  ),
+                  Text(
+                  'ER',
+                  style: TextStyle(
+                    color: Theme.of(context).accentColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                  )
+              ],
+            )
+          ),
+        ),
+    );
+  }
+}
